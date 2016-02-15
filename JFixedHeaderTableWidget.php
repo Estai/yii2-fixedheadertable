4ff7d04e720ce13b30678b0171609b46478575fc
etboranov <estai_05_93@mail.ru>
2016-02-15 10:21:38 +0600

    first commit

---
+++ JFixedHeaderTableWidget.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace estai\fixedheadert;
+
+use yii\base\Widget;
+use estai\fixedheadert\assets\JFixedHeaderTableAsset;
+use nsept\mousewheel\MousewheelAsset;
+use yii\helpers\Json;
+use yii\web\View;
+
+class JFixedHeaderTableWidget extends Widget {
+
+    public $container = '.table';
+    public $mousewheel = true;
+    public $settings = [];
+
+    public function run()
+    {
+        return $this->registerJFixedHeaderTableJs();
+    }
+
+    private function registerJFixedHeaderTableJs()
+    {
+        $jQueryContainer = "$('{$this->container}')";
+
+        JFixedHeaderTableAsset::register($this->view);
+            MousewheelAsset::register($this->view);
+        
+
+        if(!empty($this->settings)) {
+            $var = uniqid('$container');
+            $query = "var {$var} = {$jQueryContainer};" . PHP_EOL;
+
+            $options = Json::encode($this->settings);
+
+            return $this->view->registerJs($query . "{$var} . fixedHeaderTable({$options})", View::POS_READY);
+        }
+
+        return $this->view->registerJs($jQueryContainer . " . fixedHeaderTable()", View::POS_READY);
+    }
+}
---
+++ assets/JFixedHeaderTable.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace estai\fixedheadert\assets;
+use yii\web\AssetBundle;
+
+class JFixedHeaderTableAsset extends AssetBundle
+{
+    public $sourcePath = '@bower/jquery.fixedheadertable';
+
+    public $js = [
+        'jquery.fixedheadertable.min.js'
+    ];
+
+    public $depends = [
+        'yii\web\JqueryAsset',
+        'nsept\mousewheel\MousewheelAsset',
+    ];
+}
